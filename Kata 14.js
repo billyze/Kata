09711e148ec7d16a4d7b526b707cc00985300907
unknown <billyzedan123@gmail.com>
2020-08-30 14:30:59 -0400

    daily kata

---
+++ Kata 14.js
@@ -0,0 +1,7 @@
+//https://www.codewars.com/kata/5174a4c0f2769dd8b1000003/train/javascript
+
+function solution(nums){
+    if(nums !== null)
+      return nums.sort(function(a,b){return a-b})
+    return []
+  }
